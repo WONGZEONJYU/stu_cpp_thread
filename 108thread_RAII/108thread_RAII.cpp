6f8a371a887f2cd5ca8f32d1c6fbd7a1a7ced4d4
WONGZEONJYU <958323726@qq.com>
2023-07-12 23:41:31 +0800

    add & modified

--- 108thread_RAII/108thread_RAII.cpp
+++ 108thread_RAII/108thread_RAII.cpp
@@ -3,39 +3,19 @@
 #include <thread>
 #include <mutex>
 #include <shared_mutex>
+#include "XMutex.h"
 
 //Linux -lpthread
 using namespace std;
 using namespace chrono;
 using namespace this_thread;
 
-/*RAII*/
-class XMutex
-{
-	XMutex(const XMutex&) = delete;
-	XMutex& operator =(const XMutex&) = delete;
-
-public:
-	explicit XMutex(mutex& mux) :mux_{ mux } {
-		cout << "Lock\n";
-		mux.lock();
-	}
-
-	~XMutex()noexcept {
-		cout << "Unlock\n";
-		mux_.unlock();
-	}
-
-private:
-	mutex& mux_;
-};
-
 static mutex mux;
 
 void TestMutex(const int status)
 {
 	XMutex lock(mux);
-
+	
 	if (1 == status){
 		cout << "==1\n";
 	}else {
@@ -74,6 +54,46 @@ void TestLockGuard(const int i)
 
 int main(int argc, char* argv[])
 {
+	/*3 unique_lock 特性结合源码更好理解*/
+	/*根据项目需求去选择哪一种*/
+	{
+		static mutex mux_1;
+		{
+			/*临时解锁，一般在容器转移数据的时候会用到*/
+			unique_lock<mutex> lock(mux_1);
+			/*unique_lock构造时会加锁，退出栈区会解锁（unique_lock析构时会释放锁）*/
+			lock.unlock();/*临时释放锁，如果不再加锁，unique_lock析构不需要解锁*/
+			lock.lock();
+			
+		}
+
+		{
+			mux_1.lock();
+			unique_lock<mutex> lock(mux_1, adopt_lock);
+			/*已经拥有锁 不锁定，退出栈区解锁（unique_lock析构时解锁）*/
+		}
+
+		{
+			unique_lock<mutex> lock(mux_1, defer_lock);
+			/*推迟加锁（延后加锁） 构造后不会去获得锁 退出栈区不解锁（unique_lock析构时不解锁）*/
+			lock.lock();
+			/*加锁，退出栈区解锁（unique_lock析构时会解锁）*/
+		}
+
+		{
+			//mux_1.lock(); /*此处模拟被其他线程已经获得锁*/
+			unique_lock<mutex> lock(mux_1,try_to_lock);
+			/*尝试加锁 不阻塞 失败不拥有锁 退出栈区不解锁（unique_lock析构时不解锁）*/
+			/*假如加锁成功 退出栈区解锁（unique_lock析构时解锁）*/
+			if (lock.owns_lock() /*lock.operator bool()*/  /*lock*/ ) {
+				cout << "owns_lock\n";
+			}else {
+				cout << "not owns_lock\n";
+			}
+		}
+	}
+	(void)getchar();
+
 	/*2*/
 	for (int i = 0; i < 3; i++){
 
